8185f8b8b90ba8453b0c0e2885c6f76dd510c6e2
BlackBhul <65976935+BlackBhul@users.noreply.github.com>
2026-01-31 22:44:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
+// smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+
+    const target = document.querySelector(this.getAttribute('href'));
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+
+    // 🔥 CHIUDE MENU MOBILE
+    nav.classList.remove("active");
+    document.body.classList.remove("menu-open");
   });
 });
 
 const hamburger = document.getElementById("hamburger");
 const nav = document.querySelector(".nav-links");
 
+// toggle hamburger
 hamburger.addEventListener("click", () => {
   nav.classList.toggle("active");
   document.body.classList.toggle("menu-open");
